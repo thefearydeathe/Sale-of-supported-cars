70fd4cb635feb7048ae480f0d8c4545389649c66
sortt <kostya.lapin.2020@bk.ru>
2026-04-03 14:55:43 +0400

    добавил нормалный список

--- Sale of supported cars/lib.h
+++ Sale of supported cars/lib.h
@@ -1,14 +1,8 @@
 #pragma once
 #include <string>
 #include <Windows.h>
-struct ListItem
-{
-    int id;           // идентификатор 
-    char name[30];    // название
-    float price;      // цена за единицу
-    float count;      // количество
-    ListItem* next;   // указатель на следующий элемент
-};
+
+
 using namespace std;
 
 namespace crt 
